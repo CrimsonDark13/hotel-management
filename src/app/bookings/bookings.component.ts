a7ce484ef058e7d6476e899af6e0d0d09fbccb5b
jaisas-jessy18 <jaisas.jessy3@gmail.com>
2021-11-20 01:14:41 +0530

    updated booking and staff pages, not styled

--- src/app/bookings/bookings.component.ts
+++ src/app/bookings/bookings.component.ts
@@ -3,13 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-bookings',
   templateUrl: './bookings.component.html',
-  styleUrls: ['./bookings.component.css']
+  styleUrls: ['./bookings.component.css'],
 })
 export class BookingsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  guest = [
+    {
+      bookid: '202',
+      name: 'Fatima Doe',
+      checkin: '11/11/2021',
+      checkout: '13/11/2021',
+      roomType: 'Suite',
+    },
+    {
+      bookid: '202',
+      name: 'Fatima Doe',
+      checkin: '11/11/2021',
+      checkout: '13/11/2021',
+      roomType: 'Suite',
+    },
+    {
+      bookid: '202',
+      name: 'Fatima Doe',
+      checkin: '11/11/2021',
+      checkout: '13/11/2021',
+      roomType: 'Suite',
+    },
+  ];
 }
--- src/app/staff/staff.component.ts
+++ src/app/staff/staff.component.ts
@@ -3,13 +3,48 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-staff',
   templateUrl: './staff.component.html',
-  styleUrls: ['./staff.component.css']
+  styleUrls: ['./staff.component.css'],
 })
 export class StaffComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  staff = [
+    {
+      id: '101',
+      name: 'Fatima',
+      designation: 'Cleaner',
+      gender: 'Male',
+      doj: '11/11/2021',
+    },
+    {
+      id: '101',
+      name: 'Fatima',
+      designation: 'Cleaner',
+      gender: 'Male',
+      doj: '11/11/2021',
+    },
+    {
+      id: '101',
+      name: 'Fatima',
+      designation: 'Cleaner',
+      gender: 'Male',
+      doj: '11/11/2021',
+    },
+    {
+      id: '101',
+      name: 'Fatima',
+      designation: 'Cleaner',
+      gender: 'Male',
+      doj: '11/11/2021',
+    },
+    {
+      id: '101',
+      name: 'Fatima',
+      designation: 'Cleaner',
+      gender: 'Male',
+      doj: '11/11/2021',
+    },
+  ];
 }
