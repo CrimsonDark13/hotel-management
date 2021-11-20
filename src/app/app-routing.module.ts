b9de0b3c4cf45a9bffa3b488a79938f98b42f4f3
Smilewin <smilewinhaveluck@gmail.com>
2021-11-20 11:11:43 +0530

    removed inv and sidebar route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { GuestsComponent } from './guests/guests.component';
 import { LoginComponent } from './login/login.component';
 import { RoomsComponent } from './rooms/rooms.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { StaffComponent } from './staff/staff.component';
 
 const routes: Routes = [
   { path:'', component: LoginComponent},
-  { path:'dashboard', component: DashboardComponent},
-  { path:'sidebar', component: SidebarComponent},
-  
+  { path:'dashboard', component: DashboardComponent},  
   { 
     path:'dash-layout', component: DashboardLayoutComponent, children: [
       { path:'guests', component: GuestsComponent},
