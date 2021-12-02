f0dd895cd6f3db832e21fd67dac63a611c9cc9c5
jaisas-jessy18 <jaisas.jessy3@gmail.com>
2021-12-02 16:39:49 +0530

    fixed guests component

--- src/app/guests/guests.component.ts
+++ src/app/guests/guests.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface guestInfo {
+  name: String;
+  contactNumber: String;
+  bookingInfo: {
+    check_in: Date;
+    check_out: Date;
+    roomType: String;
+  };
+
+  _id?: String;
+}
 
 @Component({
   selector: 'app-guests',
   templateUrl: './guests.component.html',
-  styleUrls: ['./guests.component.css']
+  styleUrls: ['./guests.component.css'],
 })
 export class GuestsComponent implements OnInit {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  guests: guestInfo[] = [];
 
   ngOnInit(): void {
+    this.http
+      .get<guestInfo[]>('http://localhost:3000/hotel/api/v1/bookings')
+      .subscribe((responseData) => {
+        console.log(responseData);
+        this.guests = responseData;
+      });
   }
-
 }
